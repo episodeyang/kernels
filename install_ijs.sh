c3a061e414f9188b92018078dfabecdbb07605d0
EC2 Default User <ec2-user@ip-172-31-47-67.ec2.internal>
2015-09-11 16:57:23 +0000

    add zmq installation script

---
+++ install_ijs.sh
@@ -0,0 +1,31 @@
+sudo yum install libzmq-dev python-dev python-pip g++ curl
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+
+# install nodejs from enterprise distribution
+## run as root.
+curl --silent --location https://rpm.nodesource.com/setup | sudo bash -
+sudo yum install -y nodejs
+sudo yum install gcc-c++ make
+
+sudo pip install --upgrade ipython jinja2 tornado jsonschema pyzmq
+conda install -y pyzmq
+
+# now before you can use it, you need to upgrade the npm since CentOS's npm EPEL is not up to date.
+# install epel
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+
+# install stuff to build
+yum install -y git svn cpp make autoconf automake patch cmake wget mlocate rpm-build gcc-c++ uuid-devel pkgconfig libtool python-devel openpgm
+
+# install zeromq..
+wget http://download.zeromq.org/zeromq-4.0.5.tar.gz
+tar xzvf zeromq-4.0.5.tar.gz
+cd zeromq-4.0.5
+./configure
+make
+sudo make install
+cd ..
+sudo rm -rf zeromq-4.0.5.tar.gz
+
+sudo npm install --upgrade npm n -g
+sudo npm install -g ijavascript
--- install_jupyter.sh
+++ install_jupyter.sh
@@ -1,2 +1,6 @@
 wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
 bash Anaconda-2.3.0-Linux-x86_64.sh -b
+echo 'export PATH="/home/ec2-user/anaconda/bin:$PATH"' >> ~/.bashrc 
+
+conda install jupyter
+conda update jupyter
